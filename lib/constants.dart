00b2d6726daf35b54c3b7b838931ebb16d56ea41
Jamal-Besmar <76167410+Jamal-Besmar@users.noreply.github.com>
2022-11-01 20:54:20 +0000

    add fonts

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+const KPrimaryColor = Color(0xFF00ACC1);
+const KPrimaryLightColor = Color(0xFFFFECDE);
+const KPrimaryGradienColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFF006064),Color(0xFF00B8D4)],
+);
+const KSecondaryColor=Color(0xFF84FFFF);
+const KTextColor= Color(0xFF263238);
